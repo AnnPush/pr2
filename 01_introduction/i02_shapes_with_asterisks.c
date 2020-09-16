3549a15ceaab465104e57eccbe19a7a2f98e71f5
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-09-16 11:25:10 +0300

    Update and rename i02.c to i02_shapes_with_asterisks.c

---
+++ 01_introduction/i02_shapes_with_asterisks.c
@@ -0,0 +1,16 @@
+/*(Shapes with Asterisks) Write a program that prints the following shapes with asterisks*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("********\t\t    ***    \t\t  *  \t\t    *    \n");
+	printf("*      *\t\t  *     *  \t\t *** \t\t   * *   \n");
+	printf("*      *\t\t *       * \t\t*****\t\t  *   *  \n");
+	printf("*      *\t\t *       * \t\t  *  \t\t *     * \n");
+	printf("*      *\t\t *       * \t\t  *  \t\t*       *\n");
+	printf("*      *\t\t *       * \t\t  *  \t\t *     * \n");
+	printf("*      *\t\t *       * \t\t  *  \t\t  *   *  \n");
+	printf("*      *\t\t  *     *  \t\t  *  \t\t   * *   \n");
+	printf("********\t\t    ***    \t\t  *  \t\t    *    \n");
+}
