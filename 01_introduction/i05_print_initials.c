090acdd0c5de4ed6aa9633911d328ba3c3f0cab7
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-09-16 15:21:32 +0300

    Create i05_print_initials.c

---
+++ 01_introduction/i05_print_initials.c
@@ -0,0 +1,11 @@
+/*Print initials in block letters down the page. Construct each block letter out of the letter
+it represents. */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("PPPPPPPPP\n    P   P\n    P   P\n    P   P\n     P P \n\n");
+	printf("  JJ     \n J       \nJ        \n J       \n  JJJJJJJ\n\n");
+	printf("DDDDDDDDD\nD       D\nD       D\n D     D \n  DDDDD  \n");
+}
