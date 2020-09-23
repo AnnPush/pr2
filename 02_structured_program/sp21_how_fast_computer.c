e84be51da27a85eb941db883643784320334054a
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-09-23 11:19:27 +0300

    Create sp21_how_fast_computer.c

---
+++ 02_structured_program/sp21_how_fast_computer.c
@@ -0,0 +1,23 @@
+/*(How Fast Is Your Computer?) How can you determine how fast your own computer really
+operates? Write a program with a while loop that counts from 1 to 1,000,000,000 by 1s. Every time
+the count reaches a multiple of 100,000,000, print that number on the screen. Use your watch to
+time how long each 100 million iterations of the loop takes.*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int counter = 0;
+	int a;
+	
+	while(counter <= 1000000000)
+	{
+		counter++;
+		a = counter % 100000000;
+		if(a == 0)
+		{
+			printf("%d\n", counter);
+		}
+		
+	}
+}
