40e8fd0887f08ec7ee88f0b034e74cf93a69a667
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-09-23 11:59:10 +0300

    Create sp25_multiples_infinite_loop.c

---
+++ 02_structured_program/sp25_multiples_infinite_loop.c
@@ -0,0 +1,18 @@
+/*(Multiples of 2 with an Infinite Loop) Write a program that keeps printing the multiples of
+the integer 2, namely 2, 4, 8, 16, 32, 64, and so on. Your loop should not terminate (i.e., you should
+create an infinite loop). What happens when you run this program? */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int j = 1;
+	int a;
+	
+	while(j>0)
+	{
+		a = j * 2;
+		printf("%d\n", a);
+		++j;
+	}
+}
