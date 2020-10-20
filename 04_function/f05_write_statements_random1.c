c841d69b9529cb4c7dba9bc55136fa4fc1e0bf04
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-10-20 17:15:43 +0300

    Update f05_write_statements_random1.c

--- 04_function/f05_write_statements_random1.c
+++ 04_function/f05_write_statements_random1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main(void)
+{
+	int n, i;
+	
+	srand(time(NULL));
+	for( i = 0; i < 100; ++i)
+	{
+		//n = 2 + 2 * (rand() % 5);
+		//n = 3 + 2 * (rand() % 5);
+		n = 6 + 4 * (rand() % 5);
+
+		printf("n = %d\n", n);
+	}	
+}
