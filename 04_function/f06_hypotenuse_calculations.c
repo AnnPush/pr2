964df104ea3c63029698deb7b13c026ff3c5c11c
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-10-20 17:49:37 +0300

    Update f06_hypotenuse_calculations.c

--- 04_function/f06_hypotenuse_calculations.c
+++ 04_function/f06_hypotenuse_calculations.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <math.h>
 
+double hypotenuse(double side1, double side2);
+
+int main(void)
+{
+	double a = 2;
+	double b = 3;
+	int i;
+
+	for(i = 1; i <= 3; ++i)
+	{
+		a += i;
+		
+		if( i == 2)
+		{
+			b = (b + i) *2;
+		}
+		else
+		{
+			b +=i;
+		}
+
+		printf("Hypotenuse %d: %.2f\n", i, hypotenuse(a, b));
+	}
+}
+
+double hypotenuse(double side1, double side2)
+{
+	return sqrt(side1 *side1 + side2 * side2);
+}
