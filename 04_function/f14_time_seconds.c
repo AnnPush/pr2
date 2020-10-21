0f4f960a67ad4f68008f3bce289e3452621a0abf
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-10-21 18:21:42 +0300

    Update f14_time_seconds.c

--- 04_function/f14_time_seconds.c
+++ 04_function/f14_time_seconds.c
@@ -1,3 +1,8 @@
+/*(Time in Seconds) Write a function that takes the time as three integer arguments (for
+hours, minutes, and seconds) and returns the number of seconds since the last time the clock “struck
+12.” Use this function to calculate the amount of time in seconds between two times, both of which
+are within one 12-hour cycle of the clock.*/
+
 #include <stdio.h>
 
 int isTime(int, int, int);
