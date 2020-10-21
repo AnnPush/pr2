7b37ff9125f9ac5bca1b2aed9e1004d5a406ab46
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-10-21 14:53:34 +0300

    Update f07_exponentiation.c

--- 04_function/f07_exponentiation.c
+++ 04_function/f07_exponentiation.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+int integerPower(int, unsigned int);
+
+int main()
+{
+    int base;
+	unsigned int exponent;
+    
+    puts("Enter base:");
+    scanf("%d", &base);
+    
+    puts("Enter exponent:");
+    scanf("%u", &exponent);
+    
+    if(exponent <= 0)
+    {
+        puts("Error");
+    }
+    else
+    {
+        printf("Result: %d\n", integerPower(base, exponent));
+    }
+}    
+
+int integerPower(int b, unsigned int e)
+{
+    int p = 1;
+	int i;
+        
+    for( i = 1; i <= e; i++)
+    {
+        p *= b;
+    }
+     return p;
+}
