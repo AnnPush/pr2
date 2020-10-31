7b4518ebd68ad2d83a6b6f860df50634a731cce6
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-10-31 18:27:02 +0200

    Update f36_distance_between_point.c

--- 04_function/f36_distance_between_point.c
+++ 04_function/f36_distance_between_point.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <math.h>
 
+float fun(float, float, float, float);
+
+int main()
+{
+	float x1, x2, y1, y2;
+	printf("%s\n", "Enter x1, y1, x2, y2:");
+	scanf("%f %f %f %f", &x1, &y1, &x2, &y2);
+	
+    printf("%f", fun(x1, y1, x2, y2));
+}
+
+float fun(float a, float b, float c, float d)
+{ 
+	return sqrt((c - a) * (c - a) + (d - b) * (d - b));
+}
