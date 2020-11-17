10e15df2c2cd3dbabe82d249c034241b735f9499
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-11-17 16:47:19 +0200

    Update ar07_sum.c

--- 05_array/ar07_sum.c
+++ 05_array/ar07_sum.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #define SIZE 10
 
- int sum( int b[], int p); 
+ int sum(const int b[], int p); 
 
  
  int main(void)
@@ -18,7 +18,7 @@ int a[SIZE] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
  }
 
  
- int sum( int b[], int p)
+ int sum(const int b[], int p)
  {
 
  if (1 == p) {
