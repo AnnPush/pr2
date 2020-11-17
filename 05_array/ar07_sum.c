ef74530f5680b0da7d12cadfed8f6831559a1bf9
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-11-17 16:44:26 +0200

    Create ar07_sum.c

---
+++ 05_array/ar07_sum.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#define SIZE 10
+
+ int sum( int b[], int p); 
+
+ 
+ int main(void)
+ {
+ int x; 
+
+ 
+int a[SIZE] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+ x = sum(a, SIZE);
+
+ printf("Result is %d\n", x);
+ }
+
+ 
+ int sum( int b[], int p)
+ {
+
+ if (1 == p) {
+ return b[0];
+ } 
+else { 
+
+ return b[p - 1] + sum(b, p - 1);
+ 
+ }
+ } 
