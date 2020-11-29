0ce5e4bca6ae65198e89fa702c2d806f742e553d
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-11-29 23:59:07 +0200

    Update ar31print_string_backward.c

--- 05_array/ar31print_string_backward.c
+++ 05_array/ar31print_string_backward.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#define SIZE 10
 
+void stringReverse(char a[], char size, int i);
+
+int main(void)
+{
+	char array[SIZE] = {'d', 'y', 'n', 'o', 'j', 'l', 'p', 'r', 'y', '\0'};
+	for(int i = 0; i < SIZE; ++i)
+	{
+		printf("%5c", array[i]);
+	}
+	puts("");
+	stringReverse(array, '\0', 0);
+}
+
+void stringReverse(char a[], char size, int i)
+{
+	if(a[i] != size)
+	{
+		stringReverse(a, size, i + 1);
+		printf("%5c", a[i]);
+		
+	}
+}
