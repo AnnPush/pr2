b5a324a8bf709473f5a76383b6e57dff52a8320f
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-11-30 21:14:03 +0200

    Update ar30print_array.c

--- 05_array/ar30print_array.c
+++ 05_array/ar30print_array.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#define SIZE 5
+
+void printArray(int a[], int size, int i);
+
+int main(void)
+{
+	int array[SIZE] = {1, 7, 45, 34, 2};
+	printArray(array, SIZE, 0);
+}
+
+void printArray(int a[], int size, int i)
+{
+	if(i < size)
+	{
+	    printf("%5d", a[i]);
+		printArray(a, size, i + 1);
+		
+	}
+}
 
