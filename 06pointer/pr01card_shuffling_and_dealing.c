6de338a53cf67219fbd96a8c3701e8acf73cc75c
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-12-09 21:26:10 +0200

    Update pr01card_shuffling_and_dealing.c

--- 06pointer/pr01card_shuffling_and_dealing.c
+++ 06pointer/pr01card_shuffling_and_dealing.c
@@ -12,16 +12,19 @@
 void shuffle(unsigned int wDeck[][FACES]); // shuffling modifies wDeck
 void deal(unsigned int wDeck[][FACES], unsigned int wHand[][2], const char *wFace[], const char *wSuit[]); // dealing doesn't modify the arrays
 void oneOfPair(unsigned int wHand[][2], const char *wFace[]);
+void twoOfPair(unsigned int wHand[][2],  const char *wFace[]);
 
 int main(void)
-{
+{ //4_ инициализация колоды карт_комбинация_2_ПАРЫ_у_первого_игрока
+	 unsigned int deck[SUITS][FACES] = {28,  1, 13, 31, 32, 33,  8, 21,  5, 17, 19, 11, 20, 26, 37,  9, 50, 12, 30, 22, 24, 47,  2, 29, 15, 10, 27,  3, 16, 18, 23, 25, 34, 35, 36,  6, 38, 39, 40, 41, 14, 42, 43, 44, 45, 46, 49, 48,  4,  7, 51, 52};
+     
 	 // initialize deck array
-     unsigned int deck[SUITS][FACES] = {0};
+    // unsigned int deck[SUITS][FACES] = {0};
      // represents hand
 	 unsigned int hand[5][2];
 
 	 srand(time(NULL)); // seed random-number generator
-     shuffle(deck); // shuffle the deck
+     //shuffle(deck); // shuffle the deck
 
 	 // initialize suit array
      const char *suit[SUITS] = {"Hearts", "Diamonds", "Clubs", "Spades"};
@@ -33,6 +36,7 @@ int main(void)
 
      deal(deck, hand, face, suit); // deal the deck
      oneOfPair(hand, face);
+     twoOfPair(hand, face);
 }
 
 // shuffle cards in deck
@@ -109,3 +113,24 @@ void oneOfPair(unsigned int wHand[][2],  const char *wFace[])
 	 }
 }
 
+void twoOfPair(unsigned int wHand[][2],  const char *wFace[])
+{
+     unsigned int counter[FACES]={0};
+     
+     size_t i;
+     size_t pair;//переменная для запоминания номинала пары
+	 size_t n = 0;//количество одинаковых карт на руках у игрока
+     
+     for( i = 0; i < 5; ++i)
+     {
+         ++counter[wHand[i][1]];
+     }
+     
+     for( i = 0; i < FACES; ++i)
+     {
+		 if(counter[i] == 2 )
+         {
+             printf("\nThe hand contains a pair %s\n", wFace[i] );
+		 }
+     }  
+}
