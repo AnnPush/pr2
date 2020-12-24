1f13714d46b188e7547e405f1e25ad5eba5aedb5
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-12-24 20:43:09 +0200

    Update pr08computer_simulator.c

--- 06pointer/pr08computer_simulator.c
+++ 06pointer/pr08computer_simulator.c
@@ -13,11 +13,21 @@
 #define DIVIDE 32
 #define MULTYPLY 33
 
+#define BRANCH 40
+#define BRANCHNEG 41
+#define BRANCHZERO 42
+#define HALT 43
 
+int checkword(int word, int size);
+void dump(int acc, int icounter, int mem[]);
 
 int main(void)
 {
-	 printf("*** Симплетрон приветствует вас! ***\n"
+	int memory[SIZE] = {0};//модель памяти в качестве одномерного массива из 100 элементов
+	int accumulator = 0;//представляет аккумулятор
+	int instructionCounter = 0; //хранит адрес следующей исполняемой команды
+	
+	printf("*** Симплетрон приветствует вас! ***\n"
 	  "*** Пожалуйста, введите вашу программу по одной команде ***\n"
 	  "*** (или слову данных) за раз. Я буду выводить в качестве ***\n"
 	  "*** подсказки текущий адрес и знак вопроса (?).  ***\n"
