a6c3ac472b607cec7db9e9d4c60db9ffc483d462
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-12-28 01:39:35 +0200

    Update pr13maze_traversal.c

--- 06pointer/pr13maze_traversal.c
+++ 06pointer/pr13maze_traversal.c
@@ -2,10 +2,17 @@
 
 #define SIZE 12
 
+#define DOWN 0
+#define RIGHT 1
+#define UP 3
+#define LEFT 3
+
 void printArray(char a[][SIZE], int size);
+void mazeTraversal(char a[][SIZE], int posX, int posY, int pos);
 
 int main(void)
 {
+	
 	char arr[SIZE][SIZE] = {
 		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
 		{'#', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '#'},
@@ -21,10 +28,12 @@ int main(void)
 		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
 	};
 	
+	mazeTraversal(arr, 2, 0, RIGHT);
 	printArray(arr, SIZE);
+		
 }
 void printArray(char a[][SIZE], int size)
-{
+{puts("");
 	for(int i = 0; i < size; ++i)
 	{
 		for(int j = 0; j < size; ++j)
@@ -33,4 +42,14 @@ void printArray(char a[][SIZE], int size)
 		}
 		puts("");
 	}
+}
+
+void mazeTraversal(char a[][SIZE], int posX, int posY, int pos)
+{
+	int position = 0;
+	
+	a[posX][posY] = 'x';
+	
+	printArray(a, SIZE);
+	
 }
