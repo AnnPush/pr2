44fa54682543d58d03860e088197a6d32dcfc21e
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-01-26 13:28:34 +0200

    Update f08difference_Between_d_i.c

--- 08formatting_input_output/f08difference_Between_d_i.c
+++ 08formatting_input_output/f08difference_Between_d_i.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(void)
+{
+	int x, y;
+	
+	scanf("%i%d", &x, &y);
+	printf("%d %d\n", x, y);
+}
+
+/*
+  10   10   // 10  10
+ -10  -10   //-10 -10
+ 010  010   //  8  10
+0x10 0x10   // 16  0
+*/
