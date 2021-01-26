c181fee7754e3410ed9135089a9070fa7ff64af1
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-01-26 15:02:17 +0200

    Update f15printing_question_mark.c

--- 08formatting_input_output/f15printing_question_mark.c
+++ 08formatting_input_output/f15printing_question_mark.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main(void)
+{
+	printf("question ? %d", 100);
+}
+/*
+main. c: file not recognized: File format not recognized
+collect2: error: ld returned 1 exit status
+*/
