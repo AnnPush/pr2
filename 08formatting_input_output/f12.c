5e09ac5150caf9568f26d7354dceba7826d6dd44
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-01-26 14:38:27 +0200

    Update f12.c

--- 08formatting_input_output/f12.c
+++ 08formatting_input_output/f12.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
 
+int main(void)
+{
+	char y[20];
+	
+	//scanf("%s", y);
+	fgets(y, 20, stdin);
+	int n = strlen(y);
+	printf("n = %d\n%*s\n", n, 2 * n, y);
+
+}
+
+/*
+asd fg                                                                                                                                                                             
+n = 7                                                                                                                                                                              
+       asd fg 
+*/
