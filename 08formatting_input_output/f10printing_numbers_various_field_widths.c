bf0b7b27b1713f05c983ad7ef997d78fdcac6f0b
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-01-26 14:04:18 +0200

    Update f10printing_numbers_various_field_widths.c

--- 08formatting_input_output/f10printing_numbers_various_field_widths.c
+++ 08formatting_input_output/f10printing_numbers_various_field_widths.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main(void)
+{
+	int x = 12345;
+	float y = 1.2345;
+	
+	printf("x = %3d\n", x);
+	printf("y = %3f\n", y);
+	
+	printf("x = %1d\n", x);
+	printf("y = %1f\n", y);
+	
+	printf("x = %10d\n", x);
+	printf("y = %10f\n", y);
+}
+
+/*
+x = 12345                                                                                                                                                                          
+y = 1.234500                                                                                                                                                                       
+x = 12345                                                                                                                                                                          
+y = 1.234500                                                                                                                                                                       
+x =      12345                                                                                                                                                                     
+y =   1.234500
+*/
