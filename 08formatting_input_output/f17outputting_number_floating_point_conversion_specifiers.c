9fd8ccd4c1af48b14ef745fabb1da4d1a68131b9
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-01-31 00:48:57 +0200

    Update f17outputting_number_floating_point_conversion_specifiers.c

--- 08formatting_input_output/f17outputting_number_floating_point_conversion_specifiers.c
+++ 08formatting_input_output/f17outputting_number_floating_point_conversion_specifiers.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main(void)
+{
+	float a, b, c;
+   
+    puts("Enter the value 1.2345 three times:" );
+    scanf("%e%f%g", &a, &b, &c);
+    
+    puts("\nPrintf with %e:" );
+	printf("%e %e %e\n", a, b, c);
+	
+	puts("\nPrintf with %f:" );
+	printf("%f %f %f\n", a, b, c);
+	
+	puts("\nPrintf with %g:" );
+	printf("%g %g %g\n", a, b, c);
+	
+}
+
+/*
+Enter the value 1.2345 three times:                                                                                                                                                  
+1.2345 1.2345 1.2345                                                                                                                                                                 
+                                                                                                                                                                                     
+Printf with %e:                                                                                                                                                                      
+1.234500e+00 1.234500e+00 1.234500e+00                                                                                                                                               
+                                                                                                                                                                                     
+Printf with %f:                                                                                                                                                                      
+1.234500 1.234500 1.234500                                                                                                                                                           
+                                                                                                                                                                                     
+Printf with %g:                                                                                                                                                                      
+1.2345 1.2345 1.2345 
+*/
