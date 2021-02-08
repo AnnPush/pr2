7618477cdfb7d31fcfb2b84e035ed4edc0dbaa0b
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-02-08 23:57:09 +0200

    Update st15displaying_sentence_words_reversed.c

--- 07string/st15displaying_sentence_words_reversed.c
+++ 07string/st15displaying_sentence_words_reversed.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <string.h>
 
+#define SIZE 100
+
+void strrev(char *str, char *currentLexem);
+
+int main(void)
+{
+	char strtext[SIZE];
+	char *firstLexem;
+	
+	puts("Enter string text: ");
+	fgets(strtext, SIZE, stdin);
+	
+	firstLexem = strtok(strtext, " ");
+	strrev(strtext, firstLexem);
+	printf(" %s ", firstLexem);
+}
+
+void strrev(char *str, char *currentLexem)
+{
+
+	currentLexem = strtok(NULL, " \n");
+	
+	if(currentLexem != NULL )
+	{
+		strrev(str, currentLexem);
+		if(currentLexem != "\n")
+		printf("%s ", currentLexem);
+	}	
+}
