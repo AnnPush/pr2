13082fa579830fdc64e8b2ee829af46c1966622a
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-02-08 12:48:36 +0200

    Update st14tokenizing_telephone_numbers.c

--- 07string/st14tokenizing_telephone_numbers.c
+++ 07string/st14tokenizing_telephone_numbers.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+#define SIZE 15
+
+int main(void)
+{
+	char telephoneNumber[SIZE];
+	
+	puts("Enter telephoneNumber(example: (555)555-5555): ");
+	fgets(telephoneNumber, SIZE, stdin);
+	
+	char *currentLexem = strtok(telephoneNumber, "()-");
+	
+    int kod = strtod(currentLexem, 0);
+	printf("kod: %d\n", kod);
+	
+	currentLexem = strtok(NULL, "()-");
+	char *nom1 = currentLexem;
+		
+	currentLexem = strtok(NULL, "()-");
+	char *nom2 = currentLexem;
+		 
+    strcat(nom1, nom2);
+	
+    long telephone = strtol(nom1, 0, 0);
+    printf("telephone: %ld\n", telephone);	
+}
