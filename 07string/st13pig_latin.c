65ad6bf9e9d43f5d13db6b70a88e3c984826639e
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-02-09 14:19:19 +0200

    Update st13pig_latin.c

--- 07string/st13pig_latin.c
+++ 07string/st13pig_latin.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 100
+
+void printPigLatin(char *ptr);
+
+int main(void)
+{
+   char arr[SIZE];  
+   puts("Enter text: ");
+   fgets(arr, SIZE, stdin);
+   
+   char *arrptr;
+
+   printf("\nEnglish:  %s\n", arr);
+   printf("Pig lating:  ");
+
+   arrptr = strtok(arr, " ");
+   
+   while(arrptr != NULL) 
+   {
+      printPigLatin(arrptr);
+      arrptr = strtok(NULL, " \n");
+   }
+}
+
+void printPigLatin(char *ptr)
+{
+   char str[SIZE];
+
+   strcpy(str, ++ptr);
+
+   str[strlen(str) + 1] = '\0';
+   str[strlen(str)] = *(--ptr);
+
+   strcat(str, "ay");
+
+   printf("%s ", str);
+} 
+
 
