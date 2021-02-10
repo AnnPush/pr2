688bdbc70111badfce9ab19976b84852de237963
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-02-10 23:38:56 +0200

    Update st16searching_substrings.c

--- 07string/st16searching_substrings.c
+++ 07string/st16searching_substrings.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <string.h>
 
+#define SIZE 100
+
+int main(void)
+{
+	char arrstr[SIZE];
+	char searchstr[SIZE];
+    char *searchPtr;
+	char *s;
+	
+	puts("Enter string: ");
+	fgets(arrstr, SIZE, stdin);
+	
+    puts("Enter search string: ");
+	fgets(searchstr, SIZE, stdin);
+	s = strchr(searchstr, '\n');
+	if(s != NULL)
+		*s = '\0';
+	
+	
+	searchPtr = strstr(arrstr, searchstr);
+	if(searchPtr == NULL)
+	{
+		puts("String not");
+	}
+	else
+	{
+	printf("String1: %s\n", searchPtr);
+	}
+	searchPtr = strstr(searchPtr + 1, searchstr);
+	if(searchPtr == NULL)
+	{
+		puts("String not");
+	}
+	else{
+	printf("String2: %s\n", searchPtr);
+	}
+}
