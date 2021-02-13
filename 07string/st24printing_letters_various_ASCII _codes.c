ffdcfc5e6fd41d77949798787d93f6c8f11117c1
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-02-13 23:21:25 +0200

    Update st24printing_letters_various_ASCII _codes.c

--- 07string/st24printing_letters_various_ASCII _codes.c
+++ 07string/st24printing_letters_various_ASCII _codes.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main(void)
+{
+   int code;
+
+
+   printf("Enter ASCII code: ");
+   scanf("%d", &code);
+   printf("The character is: %c\n", code);
+/*
+   printf("ASCII code\tASCII character\n");
+   for(code = 0; code <= 255; code++) {
+      printf("%.3d\t%c\n", code, code);
+   }
+*/
+} 
