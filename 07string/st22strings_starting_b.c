f9d119c21f2d55e1dddd855d3a98c4a6bdd3a87e
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-02-13 21:47:26 +0200

    Update st22strings_starting_b.c

--- 07string/st22strings_starting_b.c
+++ 07string/st22strings_starting_b.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
+#define SIZE 127
+
+int main(void)
+{
+	char arrstr[SIZE][SIZE];
+    char *searchPtr;
+	int i = 0;
+	
+	printf("Enter string (%d to end): ", EOF);
+    fgets(arrstr[i], SIZE, stdin);
+
+    while (strtol(arrstr[i++], &searchPtr, 0) != -1 )
+    {
+        printf("Enter string (%d to end): ", EOF);
+        fgets(arrstr[i], SIZE, stdin);
+	}
+	puts("");
+	for(i = 0; i < SIZE; i++) {
+      if( tolower((int)arrstr[i][0]) == 'b')
+	 printf("%s", arrstr[i]);
+	}
+}
