2635214f6c7d68a262822bc4de44efb4f01f1f74
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-02-13 22:12:02 +0200

    Update st23strings_ending_ed.c

--- 07string/st23strings_ending_ed.c
+++ 07string/st23strings_ending_ed.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
+#define SIZE 127
+
+int main(void)
+{
+	char arrstr[SIZE][SIZE];
+    char *searchPtr;
+	char *s;
+	int i = 0;
+	
+	printf("Enter string (%d to end): ", EOF);
+    fgets(arrstr[i], SIZE, stdin);
+	
+    s = strchr(arrstr[0], '\n');
+	if(s != NULL)
+		*s = '\0';
+	
+    while (strtol(arrstr[i++], &searchPtr, 0) != -1 )
+    {
+        printf("Enter string (%d to end): ", EOF);
+        fgets(arrstr[i], SIZE, stdin);
+		
+		s = strchr(arrstr[i], '\n');
+	    if(s != NULL)
+		   *s = '\0';
+	   
+		for(int j = 0; j < (int)strlen(arrstr[i]); j++)
+	 arrstr[i][j] = tolower((int)arrstr[i][j]);
+	}
+	puts("");
+	for(i = 0; i < SIZE; i++) {
+      if( !strncmp( &arrstr[i][ strlen(arrstr[i]) - 2 ], "ed", 2 ) )
+	 printf("%s\n", arrstr[i]);
+   }
+}
