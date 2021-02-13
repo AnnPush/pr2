47af685be6e175339f662b4ca4a078024ca3c0c7
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-02-13 13:15:26 +0200

    Update st19counting_letters_alphabet_string.c

--- 07string/st19counting_letters_alphabet_string.c
+++ 07string/st19counting_letters_alphabet_string.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
+#define SIZE 127
+
+int main(void)
+{
+	char arrstr[SIZE][SIZE];
+	char * searchPtr;
+	int i = 0;
+	int counter[SIZE] = {0};
+	
+	printf("Enter string (%d to end): ", EOF);
+    fgets(arrstr[i], SIZE, stdin);
+
+    while (strtol(arrstr[i++], &searchPtr, 0) != -1 )
+    {
+        printf("Enter string (%d to end): ", EOF);
+        fgets(arrstr[i], SIZE, stdin);
+	}
+	arrstr[i - 1][0] = '\0';
+	
+	for(i = 0; i < SIZE; i++) {
+      for(int j = 0; (unsigned)j < strlen(arrstr[i]); j++) {
+	 ++counter[tolower((int)arrstr[i][j])];
+      }
+   }
+ 
+   for(i = 97; i < 122; i++) {
+      if(counter[i]) {
+         printf("%c[%d] ", i, counter[i]);
+      }
+   }
+   puts(""); 
+}
