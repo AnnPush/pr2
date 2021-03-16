7f9239f6dad19379f1f55193641d00e74c3c34b7
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-03-16 12:34:46 +0200

    Update st26write_your_string_conversion_functions.c

--- 07string/st26write_your_string_conversion_functions.c
+++ 07string/st26write_your_string_conversion_functions.c
@@ -4,35 +4,47 @@
 #include <math.h>
 
 double mystrtod(const char *nptr, char **endptr);//преобразование строки в тип double
+long mystrtol(const char *nptr, char **endptr, int base);//преобразование строки в тип long
+unsigned long mystrtoul(const char *nptr, char **endptr, int base);//преобразование строки в тип unsigned long
 
 int main(void)
 {
    char n[] = "11537463.948abc";
    char *ptr;
+   
    double num;
    long lnum;
-
+   unsigned long ulnum;
+   
    int b = 10;
 
    num = mystrtod(n, &ptr);
    printf("mystrtod(\"%s\", &ptr) = %.3f (ptr: %s)\n", n, num, ptr);
-
-  
+   
+   lnum = mystrtol(n, &ptr, b);
+   printf("\nmystrtol(\"%s\", &ptr, %d) = %ld (ptr: %s)\n", n, b, lnum, ptr);
+   
+   ulnum = mystrtoul(n, &ptr, b);
+   printf("\nmystrtoul(\"%s\", &ptr, %d) = %lu (ptr: %s)\n", n, b, ulnum, ptr);
 } 
 double mystrtod(const char *nptr, char **endptr)
 {
 	
-	int i, intValue, d = 0, result = 0;
+	int i = 0, intValue, d = 0, result = 0, k = 0;
 	double result1, m;
-	
-	for(i = 0; nptr[i] >= '0' && nptr[i] <= '9'; ++i)
+	if(nptr[0] == '-')
+	{
+		k = 1;
+		i++;
+	}
+	for(; nptr[i] >= '0' && nptr[i] <= '9'; ++i)
 	{
 		intValue = nptr[i] - '0';
 		result = result * 10 + intValue;
 	}
     
     result1 = (double)result;
-    printf("  \n%f \n", result1);
+   
     if(nptr[i] == '.')
 	{
 	   
@@ -44,14 +56,66 @@ double mystrtod(const char *nptr, char **endptr)
 			 
 			 
 			m = (double)intValue / pow(10, d);
-			 printf("  \n%f \n", m);
+			 
 		    result1 = result1 + m;
 		}
 	
 		endptr[0] = (char *) &nptr[i];
 	}
-   
+   if(k == 1)
+	{
+		result1 *= -1;
+	}
 
    return result1;
 }
+long mystrtol(const char *nptr, char **endptr, int base)
+{
+    int k = 0, i = 0, intValue;
+	long result = 0;
+	if(nptr[0] == '-')
+	{
+		k = 1;
+		i++;
+	}
+	
+	for(; nptr[i] >= '0' && nptr[i] <= '9'; ++i)
+	{
+		intValue = nptr[i] - '0';
+		result = result * 10 + intValue;
+	}
+	if(k == 1)
+	{
+		result *= -1;
+	}
+    endptr[0] = (char *) &nptr[i];
+	
+    return result;
+}
+
+unsigned long mystrtoul(const char *nptr, char **endptr, int base)
+{
+  
+    int i , intValue;
+	unsigned long result = 0;
+	
+	for(i = 0; nptr[i] >= '0' && nptr[i] <= '9'; ++i)
+	{
+		intValue = nptr[i] - '0';
+		result = result * 10 + intValue;
+	}
+	
+    endptr[0] = (char *) &nptr[i];
+	
+    return result;
+} 
+
+
+
+
+
+
+
+
+
 
