d7bfab2c798b6916c554169fd87850330bfce22a
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-04-10 19:27:23 +0300

    Update st29mystrcmp.c

--- 07string/st29mystrcmp.c
+++ 07string/st29mystrcmp.c
@@ -27,3 +27,33 @@ int mystrcmp(const char *s1, const char *s2)
         return *s1 - *s2;
     }
 }
+
+//strncmp
+#include <stdio.h>
+
+int mystrncmp(const char *s1, const char *s2, size_t n);
+
+int main(void)
+{
+  
+    printf("mystrncmp(\"abbbbc\", \"abbbbe\") = %d\n", mystrncmp("abbbbc", "abbbbe", 3) );
+    
+}
+
+int mystrncmp(const char *s1, const char *s2, size_t n)
+{
+for(int i = 0; i < n && ( ( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2 ); ++i)
+    {
+        s1++;
+        s2++;
+    }
+
+    if(*s1 == *s2)
+    {
+        return 0; 
+    }
+    else
+    {
+        return *s1 - *s2;
+    }
+}
