5ef8274d1762980f45e4c9fe0386e0a748e4c2a4
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-04-11 23:04:37 +0300

    Update st28.c

--- 07string/st28.c
+++ 07string/st28.c
@@ -1,2 +1,24 @@
-jhlhh
-jkjnjn
+#include <stdio.h>
+
+int myputs(const char *);
+
+int main(void)
+{
+    printf("Получилось %d символов.\n", myputs("qwer"));
+  
+}
+
+
+int myputs(const char * string)
+{
+    int count = 0;
+    while (*string) 
+    {
+        putchar(*string++);
+        count++;
+        printf("count: %d\n", count);
+    }
+    putchar('\n'); 
+    count++;
+    return count;
+}
