8de44d73a7130702e1ebd1a3f4d0c7e2a1bb8c07
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-04-14 23:15:41 +0300

    Update and rename st30.c to st30mystrchr.c

---
+++ 07string/st30mystrchr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+char *mystrchr(const char *s, int c);
+
+int main(void)
+{
+   char string[] = "dfgb nhyb bhmn njhb bh";
+   char search = 'n';
+   char *p;
+   int size;
+ 
+   p = mystrchr(string, search);
+   printf("mystrchr(string, search): %s\n", p);
+ }
+
+ char *mystrchr(const char *s, int c)
+{
+   int i;
+
+   for(i = 0; i < (int)strlen(s); i++) {
+      if(s[i] == c)
+	 return (char *)&s[i];
+   }
+
+   return NULL;
+}
