ac8ef1c2c1cafa1dbccb1a7d9cc007cbc63752e1
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-05-02 18:02:51 +0300

    Update sub05book.c

--- other/07struct/sub05book.c
+++ other/07struct/sub05book.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <string.h>
 
+char *s_gets(char *st, int n);
+
+#define MAXTITL 41       /*максимальная длина названия*/
+#define MAXAUTL 31       /*максимальная длина имени автора*/
+
+struct book{
+	char title[MAXTITL];
+	char author[MAXAUTL];
+	float value;
+};                       //конец шаблона стуктуры
+
+int main(void)
+{
+	struct book library; //объявление library в качестве переменной типа book
+	
+	puts("Enter title book:");
+	s_gets(library.title, MAXTITL); //доступ к разделу названия книги
+	
+	puts("Enter author book:");
+	s_gets(library.author, MAXAUTL);
+	
+	puts("Enter price book:");
+	scanf("%f", &library.value);
+	
+	
+}
