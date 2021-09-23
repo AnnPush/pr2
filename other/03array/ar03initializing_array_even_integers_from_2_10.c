b291855358aaa61a4152aaf138c864966ad3bb81
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-09-23 18:36:00 +0300

    Create ar03initializing_array_even_integers_from_2_10.c

---
+++ other/03array/ar03initializing_array_even_integers_from_2_10.c
@@ -0,0 +1,20 @@
+//Initializing the elements of array s to the even integers from 2 to 10
+#include <stdio.h>
+#define SIZE 5
+
+int main(void)
+{
+	int s[SIZE];
+
+	for ( size_t j = 0; j < SIZE; ++j )
+	{
+		s[j] = 2 + 2 * j;
+	}
+
+	printf("%s%13s\n", "Element", "Value");
+
+	for (size_t j = 0; j < SIZE; ++j )
+	{
+		printf("%7u%13d\n", j, s[j]);
+	}
+}
