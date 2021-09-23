1036f9dd0f4f624b9471471234e7e20a4787e7b5
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-09-23 18:39:53 +0300

    Create ar07roll_six_sided_times.c

---
+++ other/03array/ar07roll_six_sided_times.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 7
+
+int main(void)
+{
+	unsigned int frequency[SIZE] = {0};
+
+	for( unsigned int roll = 1; roll <= 6000; ++roll)
+	{
+		size_t face = 1 + rand() % 6;
+		++frequency[face];
+	}
+
+	printf("%s%17s\n", "Face", "Frequency");
+
+	for ( size_t face = 1; face < SIZE; ++face)
+	{
+		printf("%4d%17d\n", face, frequency[face]);
+	}
+}
