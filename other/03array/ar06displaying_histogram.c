7123ff770eca570615de6043202d156ab1e010d3
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-09-23 18:39:10 +0300

    Create ar06displaying_histogram.c

---
+++ other/03array/ar06displaying_histogram.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define SIZE 5
+
+int main()
+{
+	int n[SIZE] = {19, 3, 15, 7, 11};
+
+	printf("%s%13s%17s\n", "Element", "Value", "Histogram");
+
+	for(size_t i = 0; i < SIZE; ++i )
+	{
+		printf("%7u%13d      ", i, n[i]);
+
+		for ( int j = 1; j <= n[i]; ++j )
+		{
+			printf("%c", '*');
+		}
+
+		puts("");
+	}
+      
+}
