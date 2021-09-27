d2e14288f34c679b511de8011176060294fc7999
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-09-27 10:00:09 +0300

    Create pr04connverting_string_uppercase.c

---
+++ other/04pointer/pr04connverting_string_uppercase.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+void convertToUppercase(char *sPtr);
+
+int main(void)
+{
+	char string[] = "characters and $32.98";
+	
+	printf("The string before conversion is: %s", string);
+	convertToUppercase(string);
+	printf("\nThe string after conversion is: %s", string);
+}
+
+void convertToUppercase(char *sPtr)
+{
+	while(*sPtr != '\0')
+	{
+        if(islower(*sPtr))
+        {
+			*sPtr = toupper(*sPtr);
+        }
+		++sPtr;
+	}
+}
