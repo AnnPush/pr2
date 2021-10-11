06224873a14557417a57a9b83eaef86fae66f70f
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-10-11 14:07:17 +0300

    Update arkanoid.c

--- other/12other/arkanoid.c
+++ other/12other/arkanoid.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <conio.h>
+#include <windows.h>
 
 #define width 65
 #define height 25
 
+typedef struct{
+	int x, y;
+	int w;
+}TRacket;
+
 char mas[height][width + 1];
+TRacket racket;
+
+void initRacket()
+{
+	racket.w = 7;
+	racket.x =(width - racket.w)/2;
+	racket.y = height - 1;
+
+}
+
+void PutRaket()
+{
+	int i;
+
+	for(i = racket.x; i < racket.x + racket.w; i++)
+		mas[racket.y][i] = '@';
+}
+
 
 void init()
 {
@@ -29,11 +54,47 @@ void show()
 {
 	int i;
 	for(i = 0; i < height; i++)
-	printf("%s\n", mas[i]);
+	{
+	    printf("%s", mas[i]);
+		if(i < height - 1)
+			printf("\n");
+	}
+}
+
+void moveRacket(int x)
+{
+	racket.x = x;
+	if(racket.x < 1)
+		racket.x = 1;
+	if(racket.x + racket.w >= width)
+		racket.x = width - 1 - racket.w;
+}
+
+void setcur(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);//GetStdHandle(STD_OUTPUT_HANDLE) -указатель на наше окно
 }
 
 int main(void)
 {
-	init();
-	show();
+	char c;
+	initRacket();
+
+	do
+	{
+		setcur(0, 0); //system("cls");
+		init();
+	    PutRaket();
+	    show();
+
+		//c = getch();
+		if(GetKeyState('A') < 0) moveRacket(racket.x - 1);
+		if(GetKeyState('D') < 0) moveRacket(racket.x + 1);
+		Sleep(10);
+	}
+	while(GetKeyState(VK_ESCAPE) >= 0);
+	
 }
