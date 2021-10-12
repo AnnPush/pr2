e3f5eafb01fc4c7020377281a9cef7dc8727b9fd
AnnPush <32917082+AnnPush@users.noreply.github.com>
2021-10-12 11:38:29 +0300

    Update sub03_10_08using_unions.c

--- 09struct/sub03_10_08using_unions.c
+++ 09struct/sub03_10_08using_unions.c
@@ -1,1 +1,19 @@
-sadsgdg
+#include <stdio.h>
+
+typedef union integer {
+   char c;
+   short s;
+   int i;
+   long b;
+} Tinteder;
+
+int main(void)
+{
+   Tinteder tinteger;
+
+   printf("Char short int long: ");
+   scanf("%c%hi%d%ld", &tinteger.c, &tinteger.s, &tinteger.i, &tinteger.b);
+
+   printf("Char: %c\nShort: %hi\nInt: %d\nLong: %ld\n",
+      tinteger.c, tinteger.s, tinteger.i, tinteger.b);
+}
